f2a10f71d8663b1353117c26408d4fd638bf1fa3
IJaksic <107152550+IJaksic@users.noreply.github.com>
2023-03-01 12:01:52 +0100

    Add files via upload

---
+++ FIFO_32.v
@@ -0,0 +1,204 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:16:43 05/16/2022 
+// Design Name: 
+// Module Name:    FIFO_2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module FIFO_2(input clk_slow, reset, wr, rd, clk_internal, r_Rx_Serial, output[7:0] data_in,
+output reg[31:0] data_out, output reg empty, full, manjak, error_read, error_write, output w_Tx_Done, w_Tx_Serial
+, output reg [2:0] numData, output reg [4:0] fifo_counter); 
+
+//reg [4:0] fifo_counter;
+reg [3:0] rd_ptr, wr_ptr;
+reg [7:0] buf_mem [15:0];
+wire clk;
+reg [11:0] c_CLKS_PER_BIT = 2600;
+wire r_Tx_DV_debounced;
+wire r_Rx_DV;
+integer i;
+//reg [2:0] numData=0;
+
+debounce db(.pb_1(clk_slow), .clk(clk_internal), .pb_out(clk));
+
+uart_rx UART_RX_INST
+    (.i_Clock(clk_internal),
+     .i_Rx_Serial(r_Rx_Serial),
+     .o_Rx_DV(r_Rx_DV),
+     .o_Rx_Byte(data_in),
+	  .CLKS_PER_BIT(c_CLKS_PER_BIT)
+     );
+   
+uart_tx_32 UART_TX_INST
+    (.i_Clock(clk_internal),
+     .i_Tx_DV(clk),
+     .i_Tx_Byte(data_out),
+     .o_Tx_Active(rd),
+	  .numData(numData),
+     .o_Tx_Serial(w_Tx_Serial),
+     .o_Tx_Done(w_Tx_Done),
+	  .CLKS_PER_BIT(c_CLKS_PER_BIT)
+     );
+
+always @(fifo_counter) begin
+	empty = (fifo_counter==0 || fifo_counter > 16);
+	full = (fifo_counter==16);
+end
+
+//fifo_counter je brojaè koji broji koliko je puna memorija, u ovom bloku se provjeravaju uvjetu za //empty i full
+
+//blok koji mijenja vrijednost od fifo_counter
+always @(posedge clk) 
+begin
+if(reset)
+	fifo_counter<=0;
+else if((!full && wr) && (!empty && rd))
+	if(fifo_counter>3)
+		fifo_counter <= fifo_counter-3;
+	else
+		fifo_counter <= 1;
+		
+else if(!full && wr)
+	fifo_counter <= fifo_counter + 1;
+else if(!empty && rd)
+	if(fifo_counter>3)
+		fifo_counter <= fifo_counter - 4;
+	else
+		fifo_counter <= 0;
+		
+else if(fifo_counter>16)
+		fifo_counter<=0;
+else
+	fifo_counter <= fifo_counter;
+end
+
+always@(posedge clk)
+begin
+	manjak <= (fifo_counter<4 && rd && !empty)?1:0;
+end
+
+//blok za èitanje iz memorije
+always @(posedge clk)
+begin
+if(reset)
+	begin
+	data_out <= 0;
+	error_read <= 0;
+	numData<=0;
+	end
+else begin
+if(rd && !empty)
+	begin
+		if(fifo_counter>3) begin
+			data_out[7:0] <= buf_mem[rd_ptr];
+			data_out[15:8] <= buf_mem[rd_ptr+1];
+			data_out[23:16] <= buf_mem[rd_ptr+2];
+			data_out[31:24] <= buf_mem[rd_ptr+3];
+			numData <= 4;
+			error_read <= 0;
+		end
+		else if(fifo_counter>2) begin
+			data_out[7:0] <= buf_mem[rd_ptr];
+			//data_out[15:8] <= buf_mem[rd_ptr+1];
+			data_out[23:16] <= buf_mem[rd_ptr+1];
+			data_out[31:24] <= buf_mem[rd_ptr+2];
+			numData <= 3;
+			error_read <= 0;
+		end
+		else if(fifo_counter>1) begin
+			data_out[7:0] <= buf_mem[rd_ptr];
+			//data_out[15:8] <= buf_mem[rd_ptr+1];
+			//data_out[23:16] <= buf_mem[rd_ptr+2];
+			data_out[31:24] <= buf_mem[rd_ptr+1];
+			numData <= 2;
+			error_read <= 0;
+		end
+		else if(fifo_counter>0) begin
+			data_out[7:0] <= buf_mem[rd_ptr];
+			//data_out[15:8] <= buf_mem[rd_ptr+1];
+			//data_out[23:16] <= buf_mem[rd_ptr+2];
+			//data_out[31:24] <= buf_mem[rd_ptr+3];
+			numData <= 1;
+			error_read <= 0;
+		end
+	end
+else if (rd && empty)
+	begin
+	data_out[7:0] <= 64;
+	numData <= 1;
+	error_read <= 1;
+	end
+else 
+	begin
+   data_out <= data_out;
+	error_read <= 0;
+	end
+end
+end
+
+
+//Blok za pisanje u memoriju
+always @(posedge clk) begin
+if (reset)
+begin
+	for(i=0;i<16;i=i+1)
+			buf_mem[i]<=0;
+	error_write <= 0;
+end
+else if(wr && !full)
+	begin
+	buf_mem[wr_ptr] <= data_in;
+	error_write <= 0;
+	end
+else if(wr && full)
+	begin
+	buf_mem[wr_ptr] <= buf_mem[wr_ptr];
+	error_write <= 1;
+	end
+else 
+	begin
+	buf_mem[wr_ptr] <= buf_mem[wr_ptr];
+	error_write <= 0;
+	end
+end
+
+//Blok za mijenjanje write pointer i read pointer
+always @(posedge clk)
+begin
+if(reset)
+begin
+	wr_ptr <= 0;
+	rd_ptr <= 0;
+end
+else begin
+if(!full && wr)
+	wr_ptr <= wr_ptr + 1;
+else
+	wr_ptr <= wr_ptr;
+if(!empty && rd)
+	begin
+	if(fifo_counter>3)
+		rd_ptr <= rd_ptr + 4;
+	else
+		rd_ptr <= rd_ptr + fifo_counter;
+	end
+else
+	rd_ptr <= rd_ptr;
+end
+end
+
+
+endmodule
---
+++ uart_tx_32.v
@@ -0,0 +1,204 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////
+// File Downloaded from http://www.nandland.com
+//////////////////////////////////////////////////////////////////////
+// This file contains the UART Transmitter.  This transmitter is able
+// to transmit 8 bits of serial data, one start bit, one stop bit,
+// and no parity bit.  When transmit is complete o_Tx_done will be
+// driven high for one clock cycle.
+//
+// Set Parameter CLKS_PER_BIT as follows:
+// CLKS_PER_BIT = (Frequency of i_Clock)/(Frequency of UART)
+// Example: 10 MHz Clock, 115200 baud UART
+// (10000000)/(115200) = 87
+
+  
+module uart_tx_32
+  (
+   input       i_Clock,
+   input       i_Tx_DV,
+   input [31:0] i_Tx_Byte, 
+	input [11:0] CLKS_PER_BIT,	
+   input      o_Tx_Active,
+	input [2:0] numData,
+   output reg  o_Tx_Serial,
+   output      o_Tx_Done
+   );
+  
+  parameter s_IDLE         = 3'b000;
+  parameter s_TX_START_BIT = 3'b001;
+  parameter s_TX_DATA_BITS = 3'b010;
+  parameter s_TX_STOP_BIT  = 3'b011;
+  parameter s_CLEANUP      = 3'b100;
+   
+  reg [2:0]    r_SM_Main     = 0;
+  reg [11:0]    r_Clock_Count = 0;
+  reg [2:0]    r_Bit_Index   = 0;
+  reg [7:0]    r_Tx_Data     = 0;
+  reg          r_Tx_Done     = 0;
+  reg          r_Tx_Active   = 0;
+  reg [1:0]    brojac        = 0;
+  reg          i_Tx_DV_prethodni;
+     
+  always @(posedge i_Clock)
+    begin
+       
+      case (r_SM_Main)
+        s_IDLE :
+          begin
+            o_Tx_Serial   <= 1'b1;         // Drive Line High for Idle
+            r_Tx_Done     <= 1'b0;
+            r_Clock_Count <= 0;
+            r_Bit_Index   <= 0;
+             
+            if (i_Tx_DV == 1'b1 && i_Tx_DV_prethodni == 1'b0 && o_Tx_Active ==1)
+              begin
+                r_Tx_Active <= 1'b1;
+                r_Tx_Data   <= i_Tx_Byte [7:0];
+                r_SM_Main   <= s_TX_START_BIT;
+              end
+				else if(brojac == 1)
+						begin
+						r_Tx_Active <= 1'b1;
+                  r_Tx_Data   <= i_Tx_Byte [15:8];
+                  r_SM_Main   <= s_TX_START_BIT;
+						end
+				else if (brojac == 2)
+						begin
+						r_Tx_Active <= 1'b1;
+                  r_Tx_Data   <= i_Tx_Byte [23:16];
+                  r_SM_Main   <= s_TX_START_BIT;
+						end
+				else if(brojac == 3)
+						begin
+						r_Tx_Active <= 1'b1;
+                  r_Tx_Data   <= i_Tx_Byte [31:24];
+                  r_SM_Main   <= s_TX_START_BIT;
+						end
+            else
+              r_SM_Main <= s_IDLE;
+          end // case: s_IDLE
+         
+         
+        // Send out Start Bit. Start bit = 0
+        s_TX_START_BIT :
+          begin
+            o_Tx_Serial <= 1'b0;
+             
+            // Wait CLKS_PER_BIT-1 clock cycles for start bit to finish
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_TX_START_BIT;
+              end
+            else
+              begin
+                r_Clock_Count <= 0;
+                r_SM_Main     <= s_TX_DATA_BITS;
+              end
+          end // case: s_TX_START_BIT
+         
+         
+        // Wait CLKS_PER_BIT-1 clock cycles for data bits to finish         
+        s_TX_DATA_BITS :
+          begin
+            o_Tx_Serial <= r_Tx_Data[r_Bit_Index];
+             
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_TX_DATA_BITS;
+              end
+            else
+              begin
+                r_Clock_Count <= 0;
+                 
+                // Check if we have sent out all bits
+                if (r_Bit_Index < 7)
+                  begin
+                    r_Bit_Index <= r_Bit_Index + 1;
+                    r_SM_Main   <= s_TX_DATA_BITS;
+                  end
+                else
+                  begin
+                    r_Bit_Index <= 0;
+                    r_SM_Main   <= s_TX_STOP_BIT;
+                  end
+              end
+          end // case: s_TX_DATA_BITS
+         
+         
+        // Send out Stop bit.  Stop bit = 1
+        s_TX_STOP_BIT :
+          begin
+            o_Tx_Serial <= 1'b1;
+             
+            // Wait CLKS_PER_BIT-1 clock cycles for Stop bit to finish
+            if (r_Clock_Count < CLKS_PER_BIT-1)
+              begin
+                r_Clock_Count <= r_Clock_Count + 1;
+                r_SM_Main     <= s_TX_STOP_BIT;
+              end
+            else
+              begin
+                r_Tx_Done     <= 1'b1;
+                r_Clock_Count <= 0;
+                r_SM_Main     <= s_CLEANUP;
+                r_Tx_Active   <= 1'b0;
+              end
+          end // case: s_Tx_STOP_BIT
+         
+         
+        // Stay here 1 clock
+        s_CLEANUP :
+          begin
+				if(brojac == 0)
+					begin
+						o_Tx_Serial   <= 1'b1;         // Drive Line High for Idle
+						r_Tx_Done     <= 1'b0;
+						r_Clock_Count <= 0;
+						r_Bit_Index   <= 0;
+						if (numData == 4)
+							brojac = 1;
+						else if(numData == 1) begin
+							brojac = 0;
+							r_Tx_Done <= 1'b1;
+							r_SM_Main <= s_IDLE;
+						end
+						else
+							brojac = brojac+(5-numData);
+							
+						r_Tx_Done <= 1'b1;
+						r_SM_Main <= s_IDLE;
+					end
+				else if(brojac == 1)
+					begin
+						brojac = 2;
+						r_Tx_Done <= 1'b1;
+						r_SM_Main <= s_IDLE;
+					end
+				else if(brojac == 2)
+					begin
+						brojac = 3;
+						r_Tx_Done <= 1'b1;
+						r_SM_Main <= s_IDLE;
+					end
+				else
+					begin
+					brojac = 0;
+					r_Tx_Done <= 1'b1;
+					r_SM_Main <= s_IDLE;
+					end
+          end
+         
+         
+        default :
+          r_SM_Main <= s_IDLE;
+         
+      endcase
+		i_Tx_DV_prethodni <= i_Tx_DV;
+    end
+ 
+  assign o_Tx_Done   = r_Tx_Done;
+   
+endmodule
